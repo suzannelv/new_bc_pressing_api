f156898d3c528cae7b425a924dcc93a77d45b5d4
meijuan <lvmeijuan9011@gmail.com>
2024-03-20 23:35:45 +0100

    config easyadmin

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Category;
+use App\Entity\Client;
+use App\Entity\Employee;
+use App\Entity\Material;
+use App\Entity\OrderDetail;
+use App\Entity\OrderStatus;
+use App\Entity\Payment;
+use App\Entity\Product;
+use App\Entity\ServiceOption;
+use App\Entity\User;
+use App\Entity\ZipCode;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractDashboardController
+{
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+        // return parent::index();
+
+        // Option 1. You can make your dashboard redirect to some common page of your backend
+        //
+        // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
+
+        // Option 2. You can make your dashboard redirect to different pages depending on the user
+        //
+        // if ('jane' === $this->getUser()->getUsername()) {
+        //     return $this->redirect('...');
+        // }
+
+        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
+        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
+        //
+        return $this->render('admin/admin.html.twig');
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Mr.U-Smiley Dashbord');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToCrud('Users', 'fa-solid fa-user', User::class);
+        yield MenuItem::linkToCrud('Employées', 'fa-solid fa-user-gear', Employee::class);
+        yield MenuItem::linkToCrud('Clients', 'fa-solid fa-users', Client::class);
+        yield MenuItem::linkToCrud('Catégories', 'fa-solid fa-list', Category::class);
+        yield MenuItem::linkToCrud('Produits', 'fa-solid fa-shirt', Product::class);
+        yield MenuItem::linkToCrud('Commandes', 'fa-solid fa-truck', OrderDetail::class);
+        yield MenuItem::linkToCrud('Paiements', 'fa-solid fa-credit-card', Payment::class);
+        yield MenuItem::linkToCrud('Matières', 'fa-solid fa-cotton-bureau', Material::class);
+        yield MenuItem::linkToCrud('Services', 'fa-solid fa-jug-detergent', ServiceOption::class);
+        yield MenuItem::linkToCrud('Code postale', 'fa-solid fa-map-location-dot', ZipCode::class);
+        yield MenuItem::linkToCrud('Status de commande', 'fa-solid fa-hourglass-half', OrderStatus::class);
+
+
+    }
+}
